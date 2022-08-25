3594b55eeeeaaa8866e8331faca432084ca1ea1d
Sujit Upadhyay <sujitoic@gmail.com>
2022-08-25 19:08:56 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import Header from "./components/Header";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackList from "./components/FeedbackList";
+import { FeedbackProvider } from "./context/FeedbackContext";
+import AboutIconLink from "./components/AboutIconLink";
+import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FeedbackProvider>
+      <Router>
+        <Header text="Feedback UI " />
+
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={
+                <div>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </div>
+              }
+            ></Route>
+            <Route path="/about" element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
